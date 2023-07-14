449db669858529442c06f9b274dea3ed1fae0ee2
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-07-14 14:58:52 +0200

    Added models to app kithen

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+
+class Cook(AbstractUser):
+    class Meta:
+        ordering = ["username"]
+
