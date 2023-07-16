dd1518ae05a88be5442e5a49d7c9b91b384d87bc
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-07-16 11:23:34 +0200

    Added home page (urls, views, tamplates)

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType, Ingredient
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dish = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish": num_dish,
+        "num_dish_types": num_dish_types,
+        "num_ingredients": num_ingredients,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
